2d9dbb54f3ccb6bf069d113b34b5dda09f779342
Roksana Piwowarczyk <roksana.piwowarczyk@microsoft.wsei.edu.pl>
2025-11-18 22:55:09 +0100

    Update 03.2_reports_examples.sql

--- 03.2_reports_examples.sql
+++ 03.2_reports_examples.sql
@@ -1,24 +1,33 @@
-SELECT * FROM vw_MovieStats;
-SELECT * FROM vw_MovieRankingByMinutes;
-SELECT * FROM vw_UserSummary;
+USE PlayFilm;
+GO
 
+-- Podgląd wszystkich widoków
+SELECT * FROM dbo.vw_MovieStats;
+SELECT * FROM dbo.vw_MovieRankingByMinutes;
+SELECT * FROM dbo.vw_UserSummary;
+
+-- Top 5 najdłużej oglądanych filmów
 SELECT TOP 5 *
 FROM dbo.vw_MovieStats
-ORDER BY MinutesWatched DESC;
+ORDER BY MinutesWatched DESC, Title;
 
+-- Pełny ranking filmów wg minut oglądania
 SELECT *
 FROM dbo.vw_MovieRankingByMinutes
-ORDER BY RankByMinutes ASC;
+ORDER BY RankByMinutes ASC, Title;
 
+-- Filmy z co najmniej 3 ocenami, posortowane po średniej ocen
 SELECT *
-FROM vw_MovieStats
+FROM dbo.vw_MovieStats
 WHERE RatingCount >= 3
-ORDER BY AvgRatings DESC;
+ORDER BY AvgRatings DESC, RatingCount DESC, Title;
 
+-- Top 3 najbardziej aktywnych użytkowników
 SELECT TOP 3 * 
-FROM vw_UserSummary
-ORDER BY MinutesWatched DESC;
+FROM dbo.vw_UserSummary
+ORDER BY MinutesWatched DESC, FullName;
 
+-- Statystyki krajów wg łącznego czasu oglądania
 SELECT *
 FROM dbo.vw_CountryStats
-ORDER BY TotalMinutesWatched DESC;
+ORDER BY TotalMinutesWatched DESC, Country;
